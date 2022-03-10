efcfa7fa8e07f6519279b813bb5ed57be9d257ba
Leonel Zone Yemeli <89669494+Zoneyem@users.noreply.github.com>
2022-03-10 12:03:48 +0100

    Updated my new code

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-
-
-
-
-let contacts = document.querySelector(".contacts");
-console.log(contacts);
-let linkedinPeople = [];
-
-async function catchInfos() {
-	const response = await fetch(
-		"https://dummy-apis.netlify.app/api/contact-suggestions?count=8",
-	);
-	const collected = await response.json();
-	linkedinPeople = collected;
-
-	linkedinPeople.forEach((person) => {
-		contacts.innerHTML += `
+const contacts = document.querySelector(".contacts");
+let response = fetch(
+	"https://dummy-apis.netlify.app/api/contact-suggestions?count=8",
+)
+	.then((response) => {
+		return response.json();
+	})
+	.then((linkedinPeople) => {
+		linkedinPeople.forEach((person) => {
+			contacts.innerHTML += `
 
 			<section class="personas"  >
 
@@ -35,43 +28,37 @@ async function catchInfos() {
 			</section>
 
 			`;
-	});
+		});
 
-	let count = 0;
-	let storedCount = Number(localStorage.getItem("Pending"));
+		const buttons = document.querySelectorAll(".LinkButton");
+		let count = 0;
+		let storedCount = Number(localStorage.getItem("Pending"));
+		const counting = document.querySelector(".for-counting-invitations");
+		const deleteButtons = document.querySelectorAll(".deleteButton");
 
-	//if storedCount exist i.e storedCount !== 0 && null
-	//? ==> if
-	//the left hand side of the ":" is wat you do if the condition is true and the rigth if false
-	storedCount ? console.log("exists") : localStorage.setItem("Pending", 0);
+		function buttonChange() {
+			buttons.forEach((button, index) => {
+				button.addEventListener("click", function () {
+					button.classList.toggle("pending");
+					if (button.classList.contains("pending")) {
+						button.textContent = "Pending";
+						count += 1;
+						storedCount += 1;
+						localStorage.setItem("Pending", storedCount);
+						counting.textContent = `${storedCount} pending invitation`;
+					} else {
+						button.textContent = "Connect";
+						count -= 1;
+						storedCount -= 1;
+						localStorage.setItem("Pending", storedCount);
 
-	const counting = document.querySelector(".forCountingInvitations");
-	const sections = document.querySelectorAll("section");
-	const deleteButton = document.querySelectorAll(".deleteButton");
-	const deleteButto = document.querySelector(".deleteButton");
-	console.log(deleteButton);
-	console.log(deleteButto);
-	const section = document.querySelector(".personas");
-	const button = document.querySelectorAll(".LinkButton");
+						counting.textContent = `${storedCount} pending invitation`;
+					}
+				});
+			});
+		}
 
-	button.forEach((button, index) => {
-		button.addEventListener("click", function () {
-			button.classList.toggle("pending");
-			if (button.classList.contains("pending")) {
-				button.textContent = "Pending";
-				count += 1;
-				storedCount += 1;
-				localStorage.setItem("Pending", storedCount);
-				counting.textContent = `${storedCount} pending invitation`;
-			} else {
-				button.textContent = "Connect";
-				count -= 1;
-				storedCount -= 1;
-				localStorage.setItem("Pending", storedCount);
-
-				counting.textContent = `${storedCount} pending invitation`;
-			}
-
+		function countingModifier() {
 			if (storedCount > 1) {
 				counting.textContent = `${storedCount} pending invitations`;
 			}
@@ -79,22 +66,58 @@ async function catchInfos() {
 			if (storedCount === 0) {
 				counting.textContent = `No pending invitations`;
 			}
-		});
-	});
+		}
 
-	setTimeout(function () {
-		counting.textContent = `${storedCount} pending invitations`;
-		console.log("load event detected!");
-	}, 100);
+		const deleteProfile = function () {
+			const deleteButtons = document.querySelectorAll(".deleteButton");
+			deleteButtons.forEach((deleteButton, index) => {
+				deleteButton.addEventListener("click", function (index) {
+					deleteButton.parentElement.remove(index);
+					let response = fetch(
+						"https://dummy-apis.netlify.app/api/contact-suggestions?count=1",
+					)
+						.then((response) => {
+							return response.json();
+						})
+						.then((linkedinPeople) => {
+							linkedinPeople.forEach((person) => {
+								contacts.innerHTML += `
 
-	// counting.addEventListener('onload', )
-	// localStorage.setItem("counting", "count");
+			<section class="personas"  >
 
-	deleteButton.forEach((deleteButton, index) => {
-		deleteButton.addEventListener("click", function () {
-			console.log(linkedinPeople[index]);
-		});
-	});
-}
+			<p>
 
-catchInfos();
+			<img src="${person.picture}" alt="">
+			      <h3> <b>${person.name.title} ${person.name.first} ${person.name.last}</b> </h3>
+			<p>${person.title}</p>
+
+			<p>${person.mutualConnections} mutual connections</p>
+			<i class='fa fa-close'></i>
+			  <button class="LinkButton">
+						Connect
+					</button>
+			  <button class="deleteButton">x</button>
+
+			</p>
+			</section>
+
+			`;
+							});
+						})
+
+						.catch((err) => console.error(err));
+				});
+			});
+		};
+
+		function addNewProfile() {
+			buttons;
+		}
+
+		buttonChange();
+		countingModifier();
+		deleteProfile();
+		addNewProfile();
+	})
+
+	.catch((err) => console.error(err));
